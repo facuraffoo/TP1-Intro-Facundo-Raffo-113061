19132bc7d4a46472ce43fb497cdc4f909df0efd4
facuraffoo <fraffo@fi.uba.ar>
2025-09-04 11:31:56 -0300

    Agrege un comentario hacia el usuario que notifica si no existe el pokemon

--- entregables/parte_2.sh
+++ entregables/parte_2.sh
@@ -8,8 +8,10 @@ archivo_habilidades=$(find "$directorio_inicial" -type f -name "ability_names.cs
 pedir_pokemones() {
     echo "Ingrese un pokemon o 'ctrl + d' para salir: "
     while read -r nombre_pokemon; do
+        encontrado=0
         while IFS=',' read -r id identifier species_id height weight base_experience order is_default; do
             if [[ $nombre_pokemon = $identifier ]]; then
+                encontrado=1
                 echo "-----------------------------"
                 echo "Pokemon: $identifier"
                 echo "Altura: $(( height * 10 )) cm."
@@ -27,6 +29,12 @@ pedir_pokemones() {
                 echo "-----------------------------"
             fi
         done < <(tail -n +2 "$archivo_pokemons")
+        
+        if [[ $encontrado -eq 0 ]]; then
+            echo "El pokemon '$nombre_pokemon' no existe en la base de datos."
+        fi
+
+        echo "Ingrese un pokemon o 'ctrl + d' para salir: "
     done
 }
 
